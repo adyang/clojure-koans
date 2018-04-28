fe82c3b5cbfd5f66553e5d432cb46e86956bd5f3
adyang <dunyang@thoughtworks.com>
2018-04-28 22:57:21 +0800

    Obtained clojure enlightenment.

--- src/koans/24_macros.clj
+++ src/koans/24_macros.clj
@@ -9,37 +9,40 @@
 
 (defmacro infix-concise [form]
   `(~(second form) ; Note the syntax-quote (`) and unquote (~) characters!
-    __
-    __))
+    ~(first form)
+    ~(let [third (nth form 2)]
+      (if (number? third)
+        third
+       (list (second third) (first third) (nth third 2)))))) 
 
 (defmacro recursive-infix [form]
   (cond (not (seq? form))
-        __
+        form
         (= 1 (count form))
         `(recursive-infix ~(first form))
         :else
         (let [operator (second form)
               first-arg (first form)
-              others __]
+              others (drop 2 form)]
           `(~operator
             (recursive-infix ~first-arg)
             (recursive-infix ~others)))))
 
 (meditations
   "Macros are like functions created at compile time"
-  (= __ (hello "Macros!"))
+  (= "Hello, Macros!" (hello "Macros!"))
 
   "I can haz infix?"
-  (= __ (infix (9 + 1)))
+  (= 10 (infix (9 + 1)))
 
   "Remember, these are nothing but code transformations"
-  (= __ (macroexpand '(infix (9 + 1))))
+  (= '(+ 9 1) (macroexpand '(infix (9 + 1))))
 
   "You can do better than that - hand crafting FTW!"
   (= '(* 10 2) (macroexpand '(infix-concise (10 * 2))))
 
   "Things don't always work as you would like them to... "
-  (= '(+ 10 (2 * 3)) (macroexpand '(infix-concise (10 + (2 * 3)))))
+  (= '(+ 10 (* 2 3)) (macroexpand '(infix-concise (10 + (2 * 3)))))
 
   "Really, you don't understand recursion until you understand recursion"
   (= 36 (recursive-infix (10 + (2 * 3) + (4 * 5)))))
